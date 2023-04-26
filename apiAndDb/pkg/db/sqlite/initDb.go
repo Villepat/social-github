6084718b104f5fd99e40e1aedf9d80fa78065af9
Rihizz <santeri.riihela@gmail.com>
2023-04-26 15:21:10 +0300

    autismi22

--- apiAndDb/pkg/db/sqlite/initDb.go
+++ apiAndDb/pkg/db/sqlite/initDb.go
@@ -39,19 +39,19 @@ func InitDb() {
 	}
 
 	// _, err = db.Exec(`
-	// 	CREATE TABLE IF NOT EXISTS users (
-	// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-	// 		username TEXT NOT NULL UNIQUE,
-	// 		email TEXT NOT NULL UNIQUE,
-	// 		password TEXT NOT NULL,
-	// 		key TEXT NOT NULL UNIQUE,
-	// 		name TEXT NOT NULL,
-	// 		surname TEXT NOT NULL,
-	// 		birthdate TEXT NOT NULL,
-	// 		aboutme TEXT
-	// 	);
+	//     CREATE TABLE IF NOT EXISTS users (
+	//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+	//         username TEXT NOT NULL UNIQUE,
+	//         email TEXT NOT NULL UNIQUE,
+	//         password TEXT NOT NULL,
+	//         key TEXT NOT NULL UNIQUE,
+	//         name TEXT NOT NULL,
+	//         surname TEXT NOT NULL,
+	//         birthdate TEXT NOT NULL,
+	//         aboutme TEXT
+	//     );
 	// `)
 	// if err != nil {
-	// 	log.Fatal(err)
+	//     log.Fatal(err)
 	// }
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.19
 
 require github.com/mattn/go-sqlite3 v1.14.16
 
-require github.com/gofrs/uuid v4.4.0+incompatible
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/gorilla/websocket v1.5.0
+	golang.org/x/crypto v0.8.0
+)
 
 require (
-	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 )
