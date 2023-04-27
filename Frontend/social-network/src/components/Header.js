5f835808484b177f6a610fcb50cb8f3159103315
stefanie <steffa89@hotmail.com>
2023-04-27 14:58:59 +0300

    Moving logoutbutton from header to home

--- Frontend/social-network/src/components/Header.js
+++ Frontend/social-network/src/components/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
-import Button from "./Button";
+// import Button from "./Button";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 import Cookies from "js-cookie";
+
+
+
+
+
+
+
+
+
+
+
+//----------------------Obs!!----------//
+//Not in use any more, moved to Home.js
+
 
 function Header() {
   let isLoggedIn = false;
@@ -23,13 +37,25 @@ function Header() {
         </div>
       </div>
     );
-  } else {
-    return (
-      <div>
-        <h1>Logged in</h1>
-        <Button buttonType="Logout" />
-      </div>
-    );
+  // } else {
+  //   return (
+  //     <div className="h">
+  //       <style jsx>{`
+  //         .h {
+  //           display: flex;
+  //           justify-content: space-between;
+  //           align-items: center;
+  //           padding: 0 1rem;
+  //           margin-top: 5rem;
+  //           height: 3rem;
+  //           border-bottom: 1px solid #e5e5e5;
+  //         }
+  //       `}</style>
+
+  //       <h1>Logged in</h1>
+  //       <Button buttonType="Logout" />
+  //     </div>
+    // );
   }
 }
 
--- Frontend/social-network/src/components/Home.js
+++ Frontend/social-network/src/components/Home.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import CreatePostModal from "./CreatePostModal";
 import "./Home.css";
+import Button from "./Button";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+import Cookies from "js-cookie";
+import { Link } from "react-router-dom";
+
 
 function Home() {
   const [showCreatePost, setShowCreatePost] = useState(false);
@@ -10,35 +16,70 @@ function Home() {
     // Add class to body to hide navbar
     document.body.classList.toggle("modal-open");
   };
+  let isLoggedIn = false;
+  // check if cookie named token exists
 
-  return (
-    <>
-      <div className="topnav">
-        <a className="active" href="#home">
-          Home
-        </a>
-        <a href="#profile">Profile</a>
-        <a href="#bals">Chat</a>
-        <a href="#" onClick={toggleCreatePost}>
-          Create a Post
-        </a>
-        <div className="search-container">
-          <form action="/search">
-            <input type="text" placeholder="Search.." name="search" />
-            <a href="#sus" type="submit">
-              Search
-            </a>
-          </form>
+  if (Cookies.get("token") === undefined) {
+    isLoggedIn = false;
+  } else {
+    isLoggedIn = true;
+  }
+  if (!isLoggedIn) {
+    return (
+      <div>
+        <div className="loginregister-container">
+          <LoginForm />
+          <RegisterForm />
         </div>
       </div>
-      {showCreatePost && (
-        <CreatePostModal
-          show={showCreatePost}
-          toggleCreatePost={toggleCreatePost}
-        />
-      )}
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className="topnav">
+          <Link to="/" className="active">
+            Home
+          </Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/chat">Chat</Link>
+          <Link to="#" onClick={toggleCreatePost}>
+            Create a Post
+          </Link>
+          <div className="search-container">
+            <form action="/search">
+              <input type="text" placeholder="Search.." name="search" />
+              <Link to="/search" type="submit">
+                Search
+              </Link>
+            </form>
+          </div>
+        </div>
+        {showCreatePost && (
+          <CreatePostModal
+            show={showCreatePost}
+            toggleCreatePost={toggleCreatePost}
+          />
+        )}
+        <div className="h">
+          <style jsx>{`
+            .h {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              padding: 0 1rem;
+              margin-top: 5rem;
+              height: 3rem;
+              border-bottom: 1px solid #e5e5e5;
+            }
+          `}</style>
+    
+          <h1>Logged in</h1>
+          <Button buttonType="Logout" />
+        </div>
+      </>
+    );
+  }
 }
+
 
 export default Home;
