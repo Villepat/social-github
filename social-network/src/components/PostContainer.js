8659f62395425f636a999fd3958b46a2e74ad9ea
Rihizz <santeri.riihela@gmail.com>
2023-05-03 09:50:15 +0300

    added key to postcontainer so the error is gone

--- social-network/src/components/PostContainer.js
+++ social-network/src/components/PostContainer.js
@@ -4,7 +4,7 @@ function PostContainer({ posts, setPosts }) {
   return (
     <div className="post-container">
       {posts.map((post) => (
-        <div className="post">
+        <div key={post.id} className="post">
           <h3>{post.content}</h3>
           <h4>{post.date}</h4>
           <h4>{post.full_name}</h4>
