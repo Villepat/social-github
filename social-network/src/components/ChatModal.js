228e12d14955ac7fc1e8d9e96319cecc19cf7cb7
stefanie <steffa89@hotmail.com>
2023-05-31 13:13:27 +0300

    Frontend single-chatt

--- social-network/src/components/ChatModal.js
+++ social-network/src/components/ChatModal.js
@@ -7,13 +7,14 @@ function ChatModal({ user, onClose }) {
   const [messages, setMessages] = useState([]);
   const { ws, nickname } = useAuth();
 
+
   useEffect(() => {
     if (ws) {
       ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
         setMessages((prevMessages) => [
           ...prevMessages,
-          { text: data.message, from: data.from },
+          { text: data.message, from: data.from, timestamp: new Date() },
         ]);
       };
     }
@@ -31,6 +32,7 @@ function ChatModal({ user, onClose }) {
 
   const handleSendMessage = () => {
     console.log("sender: ", nickname);
+    console.log("receiver: ", user.fullname);
     // Here you would send the message over the WebSocket connection
     if (ws.readyState === WebSocket.OPEN) {
       const payload = {
@@ -39,6 +41,7 @@ function ChatModal({ user, onClose }) {
         Command: "NEW_MESSAGE",
         message,
       };
+
 
       // Send the message over the WebSocket connection
       ws.send(JSON.stringify(payload));
@@ -52,21 +55,47 @@ function ChatModal({ user, onClose }) {
     setMessage("");
   };
 
+  //make sure the scroll position is the same as before
+  useEffect(() => {
+    const chatMessages = document.querySelector(".chat-messages");
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }, [messages]);
+  
+
+  const formatTimestamp = (dateTime) => {
+    const options = {
+      day: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+    };
+    return new Date(dateTime).toLocaleString(undefined, options);
+  };
+
+
   return (
     <div className="chat-modal">
-      <h2>Chat with {user.fullname}</h2>
-      <div className="chat-messages">
-        {messages.map((message, index) => (
-          <div key={index} className={`chat-message ${message.from}`}>
+    <button className="close-x" onClick={onClose}>X</button>
+    <h2>Chat with {user.fullname}</h2>
+    <div className="chat-messages">
+      {messages.map((message, index) => (
+        <div key={index} className={`chat-message ${message.from}`}>
+          <div className="chat-username">
+            {message.from === "me" ? "Me" : user.fullname}
+          </div>
+          <div className="chat-message-text">
             {message.text}
           </div>
-        ))}
+          <div className="chat-timestamp">
+            {formatTimestamp(message.timestamp)}
+          </div>
+        </div>
+      ))}
       </div>
       <div className="chat-input">
-        <input type="text" value={message} onChange={handleInputChange} />
-        <button onClick={handleSendMessage}>Send</button>
+        <input className="text-input" type="text" value={message} onChange={handleInputChange} />
+        <button className="send-button" onClick={handleSendMessage}>Send</button>
       </div>
-      <button onClick={onClose}>Close</button>
     </div>
   );
 }
